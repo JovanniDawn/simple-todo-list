0a691ef4b3ff53a3710562d1292c0cd4346b36c7
Jovanni Dawn Vizmanos <jovannidawn@gmail.com>
2025-04-28 21:20:09 +0800

    Initial commit

---
+++ src/components/TodoForm.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+
+export const TodoForm = ({addTodo}) => {
+  const [value, setValue] = useState("");
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    if (value.trim() === "") {
+      alert("Input a task");
+      return;
+    }
+
+    addTodo(value);
+    
+    setValue("");
+  }
+
+  return (
+    <form className="TodoForm" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        className="todo-input"
+        placeholder="Input your task"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
+      <button type="submit" className="todo-btn">
+        Add Task
+      </button>
+    </form>
+  );
+};
---
+++ src/components/TodoWrapper.js
@@ -0,0 +1,74 @@
+import React, { useState, useEffect } from "react";
+import { TodoForm } from "./TodoForm";
+import { v4 as uuidv4 } from "uuid";
+import { Todo } from "./Todo";
+import { EditTodoForm } from "./EditTodoForm";
+
+export const TodoWrapper = () => {
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (todo) => {
+    setTodos((prevTodos) => [
+      ...prevTodos,
+      { id: uuidv4(), task: todo, completed: false, isEditing: false },
+    ]);
+  };
+
+  const toggleComplete = (id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
+  };
+
+  const deleteTodo = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
+  const editTodo = (id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, isEditing: !todo.isEditing } : todo
+      )
+    );
+  };
+
+  const editTask = (task, id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id
+          ? {
+              ...todo,
+              task,
+              isEditing: !todo.isEditing,
+            }
+          : todo
+      )
+    );
+  };
+
+  useEffect(() => {
+    console.log(todos);
+  }, [todos]);
+
+  return (
+    <div className="TodoWrapper">
+      <h1>Simple Todo List App</h1>
+      <TodoForm addTodo={addTodo} />
+      {todos.map((todo, index, id) =>
+        todo.isEditing ? (
+          <EditTodoForm editTodo={editTask} task={todo} key={id} />
+        ) : (
+          <Todo
+            task={todo}
+            key={index}
+            toggleComplete={toggleComplete}
+            deleteTodo={deleteTodo}
+            editTodo={editTodo}
+          />
+        )
+      )}
+    </div>
+  );
+};
